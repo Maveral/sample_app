0182f6d15e6381ecc61fb9c0f78503c90dead93f
Maveral <maverral@wp.pl>
2011-02-23 18:46:19 +0100

    chapter 5 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-	
+
+	def logo
+		logo = image_tag("logo.png", :alt => "Moja aplikacja", :class => "round")
+	end
+
 	def title
     base_title = "Page"
     if @title.nil?
